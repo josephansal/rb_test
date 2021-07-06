8ca54bfc7d99a62902c1a580368503e756bd2eee
Ansal <joseph.ansal@vvdntech.in>
2021-07-06 12:01:19 +0530

    updated the code

--- API/index.py
+++ API/index.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python
 # -*- coding: latin-1 -*-
+from os import confstr
 from flask import Flask, request, json, jsonify
 import redis
+from flask_cors import CORS
+import json
+# Import smtplib for the actual sending function
+import smtplib
+
+# And imghdr to find the types of our images
+import imghdr
+
+# Here are the email package modules we'll need
+from email.message import EmailMessage
+
 HOST = "localhost"
 PORT = 8007
 SUBSCRIPTION_LIST = "MYLIST"
 redisClient = redis.Redis(host='localhost', port=6379, db=0)
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
@@ -16,13 +29,11 @@ def hello_world():
 
 @app.route('/save', methods=['POST'])
 def register():
-    # print(request.data)
     data = request.json
-    # print("sasas",data)
-    # email = request.data.get('email')
-    # name = request.data.get('name')
-    redisClient.rpush(SUBSCRIPTION_LIST, data)
-
+    with open('data.json', 'w') as tfile:
+        json.dump([data], tfile, ensure_ascii=False, indent=4)
+    # with open('data.json', 'w', encoding='utf-8') as f:
+    # json.dump(data, f, ensure_ascii=False, indent=4)
     response = app.response_class(
         response=json.dumps({"message": "Success", "status": 1}),
         status=200,
@@ -38,14 +49,41 @@ def export():
 
 @app.route('/list')
 def list():
-    list = redisClient.lrange(SUBSCRIPTION_LIST,0 ,-1)
-    print(list)
+    f = open('data.json')
+    data = json.load(f)
+    print(data)
+    # data = [{"sl":"1", "name": "John", "email":"john@sasa.com"}]
     response = app.response_class(
-        response=json.dumps({"message": "Success", "status": 1}),
+        response=json.dumps(
+            {"message": "Success", "status": 1, "data": json.dumps(data)}),
         status=200,
         mimetype='application/json'
     )
     return response
+
+
+def send_email(email):
+
+    # Create the container email message.
+    msg = EmailMessage()
+    msg['Subject'] = 'News letter'
+    # me == the sender's email address
+    # family = the list of all recipients' email addresses
+    msg['From'] = "josephansal@gmail.com"
+    msg['To'] = email
+    msg.preamble = 'You will not see this in a MIME-aware mail reader.\n'
+
+    # Open the files in binary mode.  Use imghdr to figure out the
+    # # MIME subtype for each specific image.
+    # for file in pngfiles:
+    #     with open(file, 'rb') as fp:
+    #         img_data = fp.read()
+    #     msg.add_attachment(img_data, maintype='image',
+    #                        subtype=imghdr.what(None, img_data))
+
+    # Send the email via our own SMTP server.
+    with smtplib.SMTP('localhost') as s:
+        s.send_message(msg)
 
 
 if __name__ == '__main__':
